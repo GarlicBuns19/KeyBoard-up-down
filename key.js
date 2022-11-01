909a599c9d5df7c6aafd5e79ef9157952fcf4d03
GarlicBuns19 <planterx26@gmail.com>
2022-11-01 15:36:24 +0200

    Fixed

--- key.js
+++ key.js
@@ -1,14 +1,14 @@
 const color = document.getElementById("color");
-const q = document.getElementById('q')
-const w = document.getElementById('w')
-const e = document.getElementById('e')
-const r = document.getElementById('r')
-const t = document.getElementById('t')
-const y = document.getElementById('y')
-const u = document.getElementById('u')
-const i = document.getElementById('i')
-const o = document.getElementById('i')
-const p = document.getElementById('i')
+const q = document.getElementById("q");
+const w = document.getElementById("w");
+const e = document.getElementById("e");
+const r = document.getElementById("r");
+const t = document.getElementById("t");
+const y = document.getElementById("y");
+const u = document.getElementById("u");
+const i = document.getElementById("i");
+const o = document.getElementById("i");
+const p = document.getElementById("i");
 
 let key = [];
 
@@ -19,26 +19,31 @@ let key = [];
 //         console.log(event)q
 //   });
 // });
+addEventListener("keydown", (event) => {
+  console.log(event.key);
+  hoverKey(event.key);
+});
+
 hoverKey = (id) => {
-    key.push(id)
-    document.getElementById(`${id}`).classList.add('hoverKey')
-    // console.log(key)
-    // if(key[0] == id){
-    // }
-    if(key.length > 1){
-        console.log(key)
-        if(key[0] !== key[1]){
-            document.getElementById(`${key[0]}`).classList.remove('hoverKey')
-            key.shift()
-        }
+  key.push(id);
+  document.getElementById(`${id}`).classList.add("hoverKey");
+  // console.log(key)
+  // if(key[0] == id){
+  // }
+
+  if (key.length > 1) {
+    console.log(`array`, key);
+    if (key[0] == id) {
+      key.shift();
+      console.log(`shift`, key);
     }
-}
-
-addEventListener('keydown',(event) => {
-    console.log(event.key)
-    hoverKey(event.key)
-})
-
-console.log(key)
-
-
+    // console.log(`arr after pop`, key);
+    if (key[0] != key.at(-1)) {
+      console.log(`removed`, key[0]);
+      document.getElementById(`${key[0]}`).classList.remove("hoverKey");
+      key.shift();
+      console.log(`next shift`, key);
+    }
+  }
+  console.log(`current arr`, key);
+};
